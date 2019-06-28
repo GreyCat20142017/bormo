d80633c460566b46361b73f1df2d2ea0fc64885b
GreyCat20142017 <nrz3siaatg81@mail.ru>
2019-06-28 22:05:21 +0300

    Updates

---
+++ precache-manifest.fbe14cee7195bfb7a38ac9ccb181a12a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "125483760df02eaa4c4cdf421ce8d211",
+    "url": "/bormo/index.html"
+  },
+  {
+    "revision": "9d20f4d81d12dcbf733c",
+    "url": "/bormo/static/css/main.4b5b0634.chunk.css"
+  },
+  {
+    "revision": "a27d52256c8606193b59",
+    "url": "/bormo/static/js/2.e6ea8252.chunk.js"
+  },
+  {
+    "revision": "9d20f4d81d12dcbf733c",
+    "url": "/bormo/static/js/main.7d712ad1.chunk.js"
+  },
+  {
+    "revision": "a6b476944d45d0f77531",
+    "url": "/bormo/static/js/runtime~main.cdb26e6f.js"
+  }
+]);
